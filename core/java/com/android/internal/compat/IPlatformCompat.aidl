bionic-linux/android_frameworks_base

Add apis for listing all compat changes
These apis are required for adding UI in the Developer options for
modifying compatibility change overrides.

Bug: 138280620
Test: builds
Change-Id: If55aa68f9bdd6bed0765324e972de3683bacb553

core/java/com/android/internal/compat/IPlatformCompat.aidl
@@ -20,6 +20,7 @@ import android.content.pm.ApplicationInfo;
 import java.util.Map;
 
 parcelable ParcelableCompatibilityChangeConfig;
+parcelable CompatChangeInfo;
 
 /**
  * Platform private API for talking with the PlatformCompat service.
@@ -115,4 +116,18 @@ interface IPlatformCompat
      *
      */
     void clearOverrides(in String packageName);
+
+    /**
+     * Get configs for an application.
+     *
+     * @param appInfo The application whose config will be returned.
+     */
+    Map getAppConfig(in ApplicationInfo appInfo);
+
+    /**
+     * List all compatibility changes.
+     *
+     * @return An array of {@link CompatChangeInfo}.
+     */
+    CompatChangeInfo[] listAllChanges();
 }

services/core/java/com/android/server/compat/PlatformCompat.java
@@ -23,12 +23,14 @@ import android.util.Slog;
 import android.util.StatsLog;
 
 import com.android.internal.compat.ChangeReporter;
+import com.android.internal.compat.CompatChangeInfo;
 import com.android.internal.compat.IPlatformCompat;
 import com.android.internal.compat.ParcelableCompatibilityChangeConfig;
 import com.android.internal.util.DumpUtils;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
+import java.util.Map;
 
 /**
  * System server internal API for gating and reporting compatibility changes.
@@ -96,6 +98,16 @@ public class PlatformCompat extends IPlatformCompat.Stub {
     protected void dump(FileDescriptor fd, PrintWriter pw, String[] args) {
         if (!DumpUtils.checkDumpAndUsageStatsPermission(mContext, "platform_compat", pw)) return;
         CompatConfig.get().dumpConfig(pw);
+    }
+
+    @Override
+    public Map getAppConfig(ApplicationInfo appInfo) {
+        return CompatConfig.get().dumpAppConfig(appInfo);
+    }
+
+    @Override
+    public CompatChangeInfo[] listAllChanges() {
+        return CompatConfig.get().dumpChanges();
     }
 
     private ApplicationInfo getApplicationInfo(String packageName) {

bluestacks/frameworks-base-a16

Test: atest CompatConfigTest

core/java/com/android/internal/compat/IPlatformCompat.aidl
@@ -17,8 +17,10 @@
 package com.android.internal.compat;
 
 import android.content.pm.ApplicationInfo;
+import java.util.Map;
 
 parcelable CompatibilityChangeConfig;
+parcelable CompatibilityChangeInfo;
 
 /**
  * Platform private API for talking with the PlatformCompat service.
@@ -146,4 +148,21 @@ interface IPlatformCompat
      *
      */
     void clearOverrides(in String packageName);
+
+    /**
+     * Get configs for an application.
+     *
+     * @param appInfo The application whose config will be returned.
+     *
+     * @return A {@link CompatibilityChangeConfig}, representing whether a change is enabled for
+     *         the given app or not.
+     */
+    CompatibilityChangeConfig getAppConfig(in ApplicationInfo appInfo);
+
+    /**
+     * List all compatibility changes.
+     *
+     * @return An array of {@link CompatChangeInfo} known to the service.
+     */
+    CompatibilityChangeInfo[] listAllChanges();
 }

services/core/java/com/android/server/compat/PlatformCompat.java
@@ -25,6 +25,7 @@ import android.util.StatsLog;
 
 import com.android.internal.compat.ChangeReporter;
 import com.android.internal.compat.CompatibilityChangeConfig;
+import com.android.internal.compat.CompatibilityChangeInfo;
 import com.android.internal.compat.IPlatformCompat;
 import com.android.internal.util.DumpUtils;
 
@@ -111,6 +112,16 @@ public class PlatformCompat extends IPlatformCompat.Stub {
     public void clearOverrides(String packageName) {
         CompatConfig config = CompatConfig.get();
         config.removePackageOverrides(packageName);
+    }
+
+    @Override
+    public CompatibilityChangeConfig getAppConfig(ApplicationInfo appInfo) {
+        return CompatConfig.get().getAppConfig(appInfo);
+    }
+
+    @Override
+    public CompatibilityChangeInfo[] listAllChanges() {
+        return CompatConfig.get().dumpChanges();
     }
 
     @Override