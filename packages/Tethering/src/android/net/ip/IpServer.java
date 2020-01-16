bionic-linux/android_frameworks_base

p2p: revise tethering handler for shared group interface support
When leaving a group, all information are erased and no group interface
is passed to tethering service.
For separate group interface, tethering could be stopped
on p2p group interface removed. For shared group interface,
i.e. management interface and group interface share one
interface, ex. p2p0, tethering has no chance to be stopped since management
interface won't be removed after leaving a group.

Bug: 141382930
Test: atest FrameworksNetTests
Test: atest FrameworksWifiTests
Change-Id: Ib611018b67c76ff79c7e6658136721090feb145b

packages/Tethering/src/android/net/ip/IpServer.java
@@ -456,7 +456,8 @@ public class IpServer extends StateMachine {
             InetAddress addr = parseNumericAddress(ipAsString);
             linkAddr = new LinkAddress(addr, prefixLen);
             ifcg.setLinkAddress(linkAddr);
-            if (mInterfaceType == ConnectivityManager.TETHERING_WIFI) {
+            if (mInterfaceType == ConnectivityManager.TETHERING_WIFI
+                    || mInterfaceType == ConnectivityManager.TETHERING_WIFI_P2P) {
                 // The WiFi stack has ownership of the interface up/down state.
                 // It is unclear whether the Bluetooth or USB stacks will manage their own
                 // state.

bluestacks/frameworks-base-a16

Test: atest TetheringTests

packages/Tethering/src/android/net/ip/IpServer.java
@@ -441,7 +441,8 @@ public class IpServer extends StateMachine {
         }
 
         final Boolean setIfaceUp;
-        if (mInterfaceType == TetheringManager.TETHERING_WIFI) {
+        if (mInterfaceType == TetheringManager.TETHERING_WIFI
+                || mInterfaceType == TetheringManager.TETHERING_WIFI_P2P) {
             // The WiFi stack has ownership of the interface up/down state.
             // It is unclear whether the Bluetooth or USB stacks will manage their own
             // state.