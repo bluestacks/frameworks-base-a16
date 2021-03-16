bionic-linux/android_frameworks_base

Public API for per-profile network preference.
This patch defines the API, but does not make it public
yet as there is no implementation yet.

Test: none so far
Change-Id: I854a952dfe35cc80847eb62f522b1667b8e9b8a0

packages/Connectivity/framework/src/android/net/IConnectivityManager.aidl
@@ -20,6 +20,7 @@ import android.app.PendingIntent;
 import android.net.ConnectionInfo;
 import android.net.ConnectivityDiagnosticsManager;
 import android.net.IConnectivityDiagnosticsCallback;
+import android.net.IOnCompleteListener;
 import android.net.IOnSetOemNetworkPreferenceListener;
 import android.net.INetworkActivityListener;
 import android.net.IQosCallback;
@@ -42,6 +43,7 @@ import android.os.Messenger;
 import android.os.ParcelFileDescriptor;
 import android.os.PersistableBundle;
 import android.os.ResultReceiver;
+import android.os.UserHandle;
 
 import com.android.connectivity.aidl.INetworkAgent;
 
@@ -215,4 +217,7 @@ interface IConnectivityManager
 
     void setOemNetworkPreference(in OemNetworkPreferences preference,
             in IOnSetOemNetworkPreferenceListener listener);
+
+    void setNetworkPreferenceForUser(in UserHandle profile, int preference,
+            in IOnCompleteListener listener);
 }

bluestacks/frameworks-base-a16

packages/Connectivity/framework/src/android/net/IConnectivityManager.aidl
@@ -20,6 +20,7 @@ import android.app.PendingIntent;
 import android.net.ConnectionInfo;
 import android.net.ConnectivityDiagnosticsManager;
 import android.net.IConnectivityDiagnosticsCallback;
+import android.net.IOnCompleteListener;
 import android.net.IOnSetOemNetworkPreferenceListener;
 import android.net.INetworkActivityListener;
 import android.net.IQosCallback;
@@ -43,6 +44,7 @@ import android.os.Messenger;
 import android.os.ParcelFileDescriptor;
 import android.os.PersistableBundle;
 import android.os.ResultReceiver;
+import android.os.UserHandle;
 
 import com.android.connectivity.aidl.INetworkAgent;
 
@@ -216,4 +218,7 @@ interface IConnectivityManager
 
     void setOemNetworkPreference(in OemNetworkPreferences preference,
             in IOnSetOemNetworkPreferenceListener listener);
+
+    void setProfileNetworkPreference(in UserHandle profile, int preference,
+            in IOnCompleteListener listener);
 }