4c1cb3144b21204cfd80d2b3958f1eee08acaeaf
Lucas Silva <lusilva@google.com>
2025-03-28 14:05:24 -0400

    Avoid transitioning to GH from dozing/AOD when occluded
    Bug: 402645064
    Test: atest FromAodTransitionInteractor
    Test: atest FromDozingTransitionInteractor
    Flag: com.android.systemui.glanceable_hub_v2
    Change-Id: Iddb908b3d5174096d3721d96f5fad499208239c1

--- packages/SystemUI/multivalentTests/src/com/android/systemui/keyguard/domain/interactor/FromAodTransitionInteractorTest.kt
+++ packages/SystemUI/multivalentTests/src/com/android/systemui/keyguard/domain/interactor/FromAodTransitionInteractorTest.kt
@@ -59,6 +59,7 @@ import com.android.systemui.keyguard.shared.model.KeyguardState.LOCKSCREEN
 import com.android.systemui.keyguard.shared.model.TransitionState
 import com.android.systemui.keyguard.shared.model.TransitionStep
 import com.android.systemui.keyguard.util.KeyguardTransitionRepositorySpySubject.Companion.assertThat
+import com.android.systemui.kosmos.advanceTimeBy
 import com.android.systemui.kosmos.collectLastValue
 import com.android.systemui.kosmos.runTest
 import com.android.systemui.kosmos.testScope
@@ -71,6 +72,7 @@ import com.android.systemui.statusbar.domain.interactor.keyguardOcclusionInterac
 import com.android.systemui.testKosmos
 import com.google.common.truth.Truth
 import junit.framework.Assert.assertEquals
+import kotlin.time.Duration.Companion.milliseconds
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.advanceTimeBy
 import kotlinx.coroutines.test.runCurrent
@@ -462,6 +464,27 @@ class FromAodTransitionInteractorTest : SysuiTestCase() {
             Truth.assertThat(currentScene).isEqualTo(CommunalScenes.Blank)
             assertThat(transitionRepository)
                 .startedTransition(from = KeyguardState.AOD, to = KeyguardState.LOCKSCREEN)
+        }
+
+    @Test
+    @EnableFlags(FLAG_GLANCEABLE_HUB_V2)
+    fun testTransitionToOccluded_onWakeUpFromAod_whenGlanceableHubEnabled() =
+        kosmos.runTest {
+            setCommunalV2Available(true)
+            fakeKeyguardRepository.setKeyguardOccluded(true)
+
+            val currentScene by collectLastValue(communalSceneInteractor.currentScene)
+            fakeCommunalSceneRepository.changeScene(CommunalScenes.Blank)
+
+            // Communal is not showing
+            Truth.assertThat(currentScene).isEqualTo(CommunalScenes.Blank)
+
+            powerInteractor.setAwakeForTest()
+            advanceTimeBy(100.milliseconds) // account for debouncing
+
+            Truth.assertThat(currentScene).isEqualTo(CommunalScenes.Blank)
+            assertThat(transitionRepository)
+                .startedTransition(from = KeyguardState.AOD, to = KeyguardState.OCCLUDED)
         }
 
     /** Regression test for b/348583197. */
