acef6bd6bf1a69444f1088610a5412f065108347
Orhan Uysal <uysalorhan@google.com>
2025-04-07 09:27:42 +0000

    If a transition happens, discard IME tasks.
    When a transtion happens that affects one of the IME target windows, do
    not try to restore the bounds of the windows.
    
    Test: atest DesktopImeHandler
    
    Test: trigger IME in a window, then swipe up to home. See no white
    surface of the window.
    
    Bug: 388570293
    
    Flag: com.android.window.flags.enable_desktop_ime_bugfix
    
    Change-Id: Idf58d423cefe72593ad222d2a2884faa1f198960

--- libs/WindowManager/Shell/src/com/android/wm/shell/dagger/WMShellModule.java
+++ libs/WindowManager/Shell/src/com/android/wm/shell/dagger/WMShellModule.java
@@ -490,7 +490,8 @@ public abstract class WMShellModule {
             Optional<TaskChangeListener> taskChangeListener,
             FocusTransitionObserver focusTransitionObserver,
             Optional<DesksTransitionObserver> desksTransitionObserver,
-            DesktopState desktopState) {
+            DesktopState desktopState,
+            Optional<DesktopImeHandler> desktopImeHandler) {
         return new FreeformTaskTransitionObserver(
                 shellInit,
                 transitions,
@@ -499,7 +500,8 @@ public abstract class WMShellModule {
                 taskChangeListener,
                 focusTransitionObserver,
                 desksTransitionObserver,
-                desktopState);
+                desktopState,
+                desktopImeHandler);
     }
 
     @WMSingleton
