7beef0e9696a64a3a43193e5ffaeb01fcd55d7d8
Evan Rosky <erosky@google.com>
2025-04-08 08:31:11 -0700

    Add tranistion combine trace-events
    A common source of transition problems is when
    multiple independent actions get combined into a
    single transition.
    
    This tries to add some trace events (and logs) around
    these situations to help with identifying hotspots and
    debugging.
    
    Bug: 325114242
    Test: Existing tests, just adding trace.
    Flag: EXEMPT adding trace
    Change-Id: Ie1180115665a992f22ab273ec1cb4c9d1e68c40b

--- services/core/java/com/android/server/wm/TransitionController.java
+++ services/core/java/com/android/server/wm/TransitionController.java
@@ -1758,6 +1758,7 @@ class TransitionController {
         WindowContainerTransaction mStartWCT;
         int mSyncId;
         TransitionInfo mInfo;
+        boolean mFromPlayer;
 
         private String buildOnSendLog() {
             StringBuilder sb = new StringBuilder("Sent Transition (#").append(mSyncId)
