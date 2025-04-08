d9cd6cba41d9edc0d3d8b35a8facabe21e1c42b6
Riddle Hsu <riddlehsu@google.com>
2025-04-08 15:23:25 +0800

    Schedule sleep transition only if screen is non-interactive
    TRANSIT_SLEEP is mainly for screen off. In case there is only
    keyguard sleep token and a display change transition happens.
    The change transition should not be disturbed by TRANSIT_SLEEP.
    
    Bug: 408450324
    Flag: EXEMPT bugfix
    Test: Fold device with Settings > Display
           > Continue using apps on fold > Swipe up to continue
          There won't be MSG_SEND_SLEEP_TRANSITION.
    Change-Id: Iad9b1e56dd7a0f6965d8f7263d75d4929f415853

--- services/core/java/com/android/server/wm/RootWindowContainer.java
+++ services/core/java/com/android/server/wm/RootWindowContainer.java
@@ -2555,8 +2555,8 @@ class RootWindowContainer extends WindowContainer<DisplayContent>
 
             if (display.mTransitionController.isShellTransitionsEnabled()
                     && !scheduledSleepTransition
-                    // Only care if there are actual sleep tokens.
-                    && displayShouldSleep && !display.mAllSleepTokens.isEmpty()) {
+                    // Only care if there are actual sleep states.
+                    && displayShouldSleep && display.isScreenSleeping()) {
                 scheduledSleepTransition = true;
 
                 if (!mHandler.hasMessages(MSG_SEND_SLEEP_TRANSITION)) {
@@ -3879,6 +3879,10 @@ class RootWindowContainer extends WindowContainer<DisplayContent>
             mDisplayId = displayId;
             mAcquireTime = SystemClock.uptimeMillis();
             mHashKey = makeSleepTokenKey(mTag, mDisplayId);
+        }
+
+        boolean isScreenOff() {
+            return DISPLAY_OFF_SLEEP_TOKEN_TAG.equals(mTag);
         }
 
         @Override
