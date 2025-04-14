2e30be6be36d9cc19a11902decb2c000c252c33f
burakov <burakov@google.com>
2025-04-14 16:08:59 +0000

    [Dual Shade] Remove the redundant coercion of the shade expand fraction.
    The `expandFraction` should be in [0..1] per its API contract, so the
    caller shouldn't have to coerce the value.
    
    Bug: 400530515
    Test: Unit tests still pass.
    Flag: com.android.systemui.scene_container
    Change-Id: I3ed890ee4f16ca7fda2cc65d1cd0c542445bbade

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/ui/viewbinder/NotificationScrollViewBinder.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/ui/viewbinder/NotificationScrollViewBinder.kt
@@ -90,11 +90,7 @@ constructor(
 
             launch { viewModel.maxAlpha.collectTraced { view.setMaxAlpha(it) } }
             launch { viewModel.shadeScrollState.collect { view.setScrollState(it) } }
-            launch {
-                viewModel.expandFraction.collectTraced {
-                    view.setExpandFraction(it.coerceIn(0f, 1f))
-                }
-            }
+            launch { viewModel.expandFraction.collectTraced { view.setExpandFraction(it) } }
             launch { viewModel.qsExpandFraction.collectTraced { view.setQsExpandFraction(it) } }
             launch { viewModel.blurRadius(maxBlurRadius).collect(view::setBlurRadius) }
             launch {
