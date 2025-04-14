4ea274b59d36aeb1bf4a01a602a3c9e49ca8b28c
Ben Lin <linben@google.com>
2025-04-14 23:28:01 +0000

    Add DesktkopState#isProjectedMode.
    Helper method for other parts of Shell, Launcher to quickly decide
    whether the device is currently in projected mode or not.
    
    Bug: 410592525
    Test: atest
    Flag: com.android.window.flags.enable_projected_display_desktop_mode
    Change-Id: Ifb621b572e348e729231b4e27e32fc0ba41e323b

--- libs/WindowManager/Shell/tests/unittest/src/com/android/wm/shell/shared/desktopmode/FakeDesktopState.kt
+++ libs/WindowManager/Shell/tests/unittest/src/com/android/wm/shell/shared/desktopmode/FakeDesktopState.kt
@@ -56,6 +56,10 @@ class FakeDesktopState : DesktopState {
         return isDesktopModeSupportedOnDisplay(display.displayId)
     }
 
+    override fun isProjectedMode(): Boolean {
+        return false
+    }
+
     override var overridesShowAppHandle: Boolean = false
 
     override var isFreeformEnabled: Boolean = false
