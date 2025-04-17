9b7376297f5eaa3e5a2aa2fdb19ee239f2de3089
Fabian Kozynski <kozynski@google.com>
2025-04-17 14:18:40 -0700

    Revert^2 "React earlier to expansion"
    This reverts commit c4397a877d682797e5f481dcb2e5806fc89f0b27.
    
    Reason for revert: flaky tests addressed
    Fixes: 407857406
    
    Change-Id: If26b93a88662139572171d04f59dd7447f1db5b3

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -3237,8 +3237,10 @@ public final class NotificationPanelViewController implements
     @Override
     public void updateExpansionAndVisibility() {
         if (!SceneContainerFlag.isEnabled()) {
+            boolean isExpanded = isExpanded();
             mShadeExpansionStateManager.onPanelExpansionChanged(
-                    mExpandedFraction, isExpanded(), isTracking());
+                    mExpandedFraction, isExpanded, isTracking());
+            mQsController.setPanelExpanded(isExpanded);
         }
         updateVisibility();
     }
