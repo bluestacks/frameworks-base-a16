f20fb68a1f15f5a4ec98a327b472b3731efe73ff
Mark Fasheh <mfasheh@google.com>
2025-04-25 21:38:52 +0000

    Fix for b/411488376
    Loop back around for another check of the stack if we detect a quitting
    condition during nextMessage(). Otherwise we may miss an item added in
    between our stack drain and the setting of quitting to true.
    
    Bug: 411488376
    Flag: build.RELEASE_PACKAGE_MESSAGEQUEUE_IMPLEMENTATION
    Test: atest MessageQueueTest
    Change-Id: I12bc300db8c3660214769874bde08b08ea1fbc16

--- core/java/android/os/CombinedMessageQueue/MessageQueue.java
+++ core/java/android/os/CombinedMessageQueue/MessageQueue.java
@@ -801,10 +801,8 @@ public final class MessageQueue {
             if (found == null) {
                 if (getQuitting()) {
                     mNextPollTimeoutMillis = 0;
-                    // State change will be Active->Active, so can immediately return here.
-                    return null;
                 } else if (next == null) {
-                    /* No message to deliver, sleep indefinitely, unless quitting */
+                    /* No message to deliver, sleep indefinitely */
                     mNextPollTimeoutMillis = -1;
                     nextOp = sStackStateParked;
                     if (DEBUG) {
