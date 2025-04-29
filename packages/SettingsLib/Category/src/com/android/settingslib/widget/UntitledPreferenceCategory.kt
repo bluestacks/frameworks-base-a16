38d0924a49343f6393bd60a79689178e7a960f65
Jacky Wang <jiannan@google.com>
2025-04-29 16:27:31 +0800

    [Catalyst] Split Theme/SettingsTitlelessPreferenceCategory to Category/UntitledPreferenceCategory
    Remove SettingsTitlelessPreferenceCategory breaks permission controller
    module, will do cleanup later.
    
    Fix: 414528310
    Flag: EXEMPT refactor
    Test: manual
    Change-Id: I1e94babd86da930bc19fe0ca4ae91eaebf50b752

--- packages/SettingsLib/Category/src/com/android/settingslib/widget/UntitledPreferenceCategory.kt
+++ packages/SettingsLib/Category/src/com/android/settingslib/widget/UntitledPreferenceCategory.kt
@@ -20,27 +20,29 @@ import android.content.Context
 import android.util.AttributeSet
 import androidx.preference.PreferenceCategory
 import androidx.preference.PreferenceViewHolder
-import com.android.settingslib.widget.theme.R
+import com.android.settingslib.widget.category.R
 
-class SettingsTitlelessPreferenceCategory @JvmOverloads constructor(
+/** A [PreferenceCategory] that has no title. */
+class UntitledPreferenceCategory
+@JvmOverloads
+constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0,
-    defStyleRes: Int = 0
+    defStyleRes: Int = 0,
 ) : PreferenceCategory(context, attrs, defStyleAttr, defStyleRes) {
 
     init {
         layoutResource =
-            if (SettingsThemeHelper.isExpressiveTheme(context)) {
-                R.layout.settingslib_expressive_preference_category_no_title
-            } else {
-                R.layout.settingslib_preference_category_no_title
+            when (SettingsThemeHelper.isExpressiveTheme(context)) {
+                true -> R.layout.settingslib_expressive_untitled_preference_category
+                else -> R.layout.settingslib_untitled_preference_category
             }
     }
 
     override fun onBindViewHolder(holder: PreferenceViewHolder) {
         super.onBindViewHolder(holder)
-        holder.setDividerAllowedAbove(false)
-        holder.setDividerAllowedBelow(false)
+        holder.isDividerAllowedAbove = false
+        holder.isDividerAllowedBelow = false
     }
 }
