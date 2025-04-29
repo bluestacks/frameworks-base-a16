2f2d7f50d74fa75e047a6dc2091cb492de207d92
Merissa Mitchell <liyingtan@google.com>
2025-04-28 17:31:54 -0700

    [PiP on Desktop] Always expand PiP to fullscreen if mid-recents.
    This is a workaround for an issue where expanding PiP while in Recents
    during Desktop session will lead to the device getting stuck in a bad
    state. This is a temporary fix by forcing PiP to expand to fullscreen
    always, if we are expanding while in Recents in a Desktop session.
    
    Bug: 409201669
    Test: atest PipDesktopStateTest
    Flag: com.android.window.flags.enable_desktop_windowing_pip
    
    Change-Id: Ibcdc47054aea52092341e55e2ee435543db58d5d

--- libs/WindowManager/Shell/src/com/android/wm/shell/dagger/pip/Pip2Module.java
+++ libs/WindowManager/Shell/src/com/android/wm/shell/dagger/pip/Pip2Module.java
@@ -61,6 +61,7 @@ import com.android.wm.shell.pip2.phone.PipTouchHandler;
 import com.android.wm.shell.pip2.phone.PipTransition;
 import com.android.wm.shell.pip2.phone.PipTransitionState;
 import com.android.wm.shell.pip2.phone.PipUiStateChangeController;
+import com.android.wm.shell.recents.RecentsTransitionHandler;
 import com.android.wm.shell.shared.annotations.ShellMainThread;
 import com.android.wm.shell.shared.desktopmode.DesktopState;
 import com.android.wm.shell.splitscreen.SplitScreenController;
@@ -277,12 +278,14 @@ public abstract class Pip2Module {
     @Provides
     static PipDesktopState providePipDesktopState(
             PipDisplayLayoutState pipDisplayLayoutState,
+            RecentsTransitionHandler recentsTransitionHandler,
             Optional<DesktopUserRepositories> desktopUserRepositoriesOptional,
             Optional<DragToDesktopTransitionHandler> dragToDesktopTransitionHandlerOptional,
             RootTaskDisplayAreaOrganizer rootTaskDisplayAreaOrganizer
     ) {
-        return new PipDesktopState(pipDisplayLayoutState, desktopUserRepositoriesOptional,
-                dragToDesktopTransitionHandlerOptional, rootTaskDisplayAreaOrganizer);
+        return new PipDesktopState(pipDisplayLayoutState, recentsTransitionHandler,
+                desktopUserRepositoriesOptional, dragToDesktopTransitionHandlerOptional,
+                rootTaskDisplayAreaOrganizer);
     }
 
     @WMSingleton
