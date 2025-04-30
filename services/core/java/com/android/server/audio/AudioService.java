0877766407427f1aea91703ab022075379f0d251
Oscar Azucena <oscarazu@google.com>
2025-04-30 13:48:04 -0700

    Revert^2 "Audio: add dump in AudioVolumeGroup and Strategy"
    073f5ba7b5b0afa72ba065263f487b1253452291
    
    Change-Id: Ic0fd9856b60206df66832dcdb966da6ed73a349b

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -13140,6 +13140,8 @@ public class AudioService extends IAudioService.Stub
         pw.println("\n");
         dumpSupportedSystemUsage(pw);
 
+        AudioProductStrategy.dump(pw);
+
         pw.println("\n");
         pw.println("\nSpatial audio:");
         pw.println("mHasSpatializerEffect:" + mHasSpatializerEffect + " (effect present)");
