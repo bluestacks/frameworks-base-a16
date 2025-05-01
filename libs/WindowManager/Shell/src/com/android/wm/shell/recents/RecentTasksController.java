63fa48888e9d6c0c2e58d76a4cc1eba3f988b317
minch <minch@google.com>
2025-05-01 18:28:35 +0000

    Let RecentTasksController be a DesktopRepository.DeskChangeListener
    Register RecentTasksController as a DeskChangeListener,
    and `notifyRecentTasksChanged` on a desk addition and removal.
    
    Launcher side change can be found at ag/33158727.
    
    Bug: 401602554
    Test: m
    Flag: com.android.window.flags.enable_multiple_desktops_frontend
    Flag: com.android.window.flags.enable_multiple_desktops_backend
    Change-Id: Id89ac754514a325d8dbf95cabf0499a3c4d742cb

--- libs/WindowManager/Shell/src/com/android/wm/shell/recents/RecentTasksController.java
+++ libs/WindowManager/Shell/src/com/android/wm/shell/recents/RecentTasksController.java
@@ -91,7 +91,8 @@ import java.util.stream.Collectors;
  */
 public class RecentTasksController implements TaskStackListenerCallback,
         RemoteCallable<RecentTasksController>, DesktopRepository.ActiveTasksListener,
-        TaskStackTransitionObserver.TaskStackTransitionObserverListener, UserChangeListener {
+        TaskStackTransitionObserver.TaskStackTransitionObserverListener, UserChangeListener,
+        DesktopRepository.DeskChangeListener {
     private static final String TAG = RecentTasksController.class.getSimpleName();
 
     // When the multiple desktops feature is disabled, all freeform tasks are lumped together into
@@ -203,8 +204,13 @@ public class RecentTasksController implements TaskStackListenerCallback,
         mShellCommandHandler.addCommandCallback("recents", mRecentsShellCommandHandler, this);
         mUserId = ActivityManager.getCurrentUser();
         mDesktopUserRepositories.ifPresent(
-                desktopUserRepositories ->
-                        desktopUserRepositories.getCurrent().addActiveTaskListener(this));
+                desktopUserRepositories -> {
+                    desktopUserRepositories.getCurrent().addActiveTaskListener(this);
+                    if (mDesktopState.enableMultipleDesktops()) {
+                        desktopUserRepositories.getCurrent().addDeskChangeListener(this,
+                                mMainExecutor);
+                    }
+                });
         mTaskStackListener.addListener(this);
         mTaskStackTransitionObserver.addTaskStackTransitionObserverListener(this,
                 mMainExecutor);
@@ -975,6 +981,26 @@ public class RecentTasksController implements TaskStackListenerCallback,
         if (previousUserRepository.getUserId() == currentUserRepository.getUserId()) return;
         previousUserRepository.removeActiveTasksListener(this);
         currentUserRepository.addActiveTaskListener(this);
+    }
+
+    @Override
+    public void onDeskAdded(int displayId, int deskId) {
+        notifyRecentTasksChanged();
+    }
+
+    @Override
+    public void onDeskRemoved(int displayId, int deskId) {
+        notifyRecentTasksChanged();
+    }
+
+    @Override
+    public void onActiveDeskChanged(int displayId, int newActiveDeskId, int oldActiveDeskId) {
+        // No-op for now, as only desk additions/removals affect the recent tasks list directly.
+    }
+
+    @Override
+    public void onCanCreateDesksChanged(boolean canCreateDesks) {
+        // No-op for now.
     }
 
     /**
