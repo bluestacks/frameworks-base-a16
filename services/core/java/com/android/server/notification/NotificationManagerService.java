dfafe04d5c10265b1d34101c1dd5fab49562bb16
Yuri Lin <yurilin@google.com>
2025-05-01 16:29:02 -0400

    Rename field bundles_allowed -> adjustment_allowed
    Bug: 415103149
    Test: NotificationAssistantsTest
    Flag: android.app.notification_classification_ui
    Change-Id: I1928e6a65a924c9eb28bc77e9c1e1f3a4783117e

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -13064,7 +13064,7 @@ public class NotificationManagerService extends SystemService {
                     NOTIFICATION_ADJUSTMENT_PREFERENCES,
                     /* optional int32 event_id = 1 */
                     NotificationPullStatsEvent.NOTIFICATION_BUNDLE_PREFERENCES_PULLED.getId(),
-                    /* optional bool bundles_allowed = 2 */ bundlesAllowed,
+                    /* optional bool adjustment_allowed = 2 */ bundlesAllowed,
                     /* repeated android.stats.notification.BundleTypes allowed_bundle_types = 3 */
                     allowedBundleTypes,
                     /* optional android.stats.notification.AdjustmentKey key = 4 */
