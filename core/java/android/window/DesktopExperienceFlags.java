2b9d625ced219a207a1d9b359e84697c01254a55
Thales Lima <tsuharesu@google.com>
2025-05-01 06:36:33 -0700

    Add empty desk flag to DesktopExperienceFlags.
    Bug: 410493575
    Test: m
    Flag: EXEMPT (flag infra)
    Change-Id: I98d308187bc907637e76047d04cc4d7dd2bb1ac0

--- core/java/android/window/DesktopExperienceFlags.java
+++ core/java/android/window/DesktopExperienceFlags.java
@@ -121,6 +121,8 @@ public enum DesktopExperienceFlags {
     ENABLE_DRAG_TO_MAXIMIZE(Flags::enableDragToMaximize, true, Flags.FLAG_ENABLE_DRAG_TO_MAXIMIZE),
     ENABLE_DYNAMIC_RADIUS_COMPUTATION_BUGFIX(Flags::enableDynamicRadiusComputationBugfix, true,
             Flags.FLAG_ENABLE_DYNAMIC_RADIUS_COMPUTATION_BUGFIX),
+    ENABLE_EMPTY_DESK_ON_MINIMIZE(Flags::enableEmptyDeskOnMinimize, false,
+            Flags.FLAG_ENABLE_EMPTY_DESK_ON_MINIMIZE),
     ENABLE_FREEFORM_BOX_SHADOWS(Flags::enableFreeformBoxShadows, false,
             Flags.FLAG_ENABLE_FREEFORM_BOX_SHADOWS),
     ENABLE_FREEFORM_DISPLAY_LAUNCH_PARAMS(Flags::enableFreeformDisplayLaunchParams, false,
