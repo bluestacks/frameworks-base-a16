a38eeefea20dbaf7f48afee1e8156fcec8902714
Eric Biggers <ebiggers@google.com>
2025-05-06 01:38:50 +0000

    Zeroize keystore password in unlockKeystore()
    Secrets derived from or unlocked by the LSKF need to be zeroized as soon
    as they are no longer needed.
    
    Bug: 320392352
    Test: atest FrameworksServicesTests:com.android.server.locksettings
    Flag: EXEMPT bugfix
    Change-Id: I82deeeb5aeaea5a83a0d6a0475a5df9d813764eb

--- services/core/java/com/android/server/locksettings/LockSettingsService.java
+++ services/core/java/com/android/server/locksettings/LockSettingsService.java
@@ -1514,7 +1514,12 @@ public class LockSettingsService extends ILockSettings.Stub {
     }
 
     private void unlockKeystore(int userId, SyntheticPassword sp) {
-        mKeyStoreAuthorization.onDeviceUnlocked(userId, sp.deriveKeyStorePassword());
+        final byte[] password = sp.deriveKeyStorePassword();
+        try {
+            mKeyStoreAuthorization.onDeviceUnlocked(userId, password);
+        } finally {
+            ArrayUtils.zeroize(password);
+        }
     }
 
     @VisibleForTesting /** Note: this method is overridden in unit tests */
