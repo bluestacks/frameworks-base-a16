6e08fffb99a4758580f0838843247a11acd6f12f
Chaohui Wang <chaohuiw@google.com>
2025-05-06 16:09:05 +0800

    Create Spa search converters
    SearchIndexableDataConverter - could cover the current Settings usages
    including SIMs page and SIM page.
    
    SpaSearchIndexablesProvider - could support search usage outside
    of Settings.
    
    Bug: 414693711
    Flag: EXEMPT refactor
    Test: manual - test Settings search
    Test: unit test
    Change-Id: Idbb7a892790f20a60187064af6a3401abeeadab6

--- packages/SettingsLib/Spa/search/src/com/android/settingslib/spa/search/SpaSearchRepository.kt
+++ packages/SettingsLib/Spa/search/src/com/android/settingslib/spa/search/SpaSearchRepository.kt
@@ -17,117 +17,51 @@
 package com.android.settingslib.spa.search
 
 import android.content.Context
-import android.provider.SearchIndexableResource
 import android.util.Log
-import com.android.settingslib.search.Indexable
-import com.android.settingslib.search.SearchIndexableData
-import com.android.settingslib.search.SearchIndexableRaw
 import com.android.settingslib.spa.framework.common.SettingsPageProvider
-import com.android.settingslib.spa.framework.common.SpaEnvironment
 import com.android.settingslib.spa.framework.common.SpaEnvironmentFactory
 import com.android.settingslib.spa.search.SearchablePage.SearchItem
 import com.android.settingslib.spa.search.SpaSearchLanding.SpaSearchLandingKey
 import com.android.settingslib.spa.search.SpaSearchLanding.SpaSearchLandingSpaPage
 
-class SpaSearchRepository(
-    private val spaEnvironment: SpaEnvironment = SpaEnvironmentFactory.instance
-) {
-    /**
-     * Gets the search indexable data list
-     *
-     * @param intentAction The Intent action for the search landing activity.
-     * @param intentTargetClass The Intent target class for the search landing activity.
-     */
-    fun getSearchIndexableDataList(
-        intentAction: String,
-        intentTargetClass: String,
-    ): List<SearchIndexableData> {
-        Log.d(TAG, "getSearchIndexableDataList")
-        return spaEnvironment.pageProviderRepository.value.getAllProviders().mapNotNull { page ->
-            if (page is SearchablePage) {
-                page.createSearchIndexableData(
-                    intentAction = intentAction,
-                    intentTargetClass = intentTargetClass,
-                    getPageTitleForSearch = page::getPageTitleForSearch,
-                    searchItemsProvider = page::getSearchItems,
-                )
-            } else null
-        }
+class SpaSearchRepository() {
+    /** Gets the search indexable data list */
+    fun getSearchIndexablePageList(): List<SpaSearchIndexablePage> {
+        Log.d(TAG, "getSearchIndexablePage")
+        return SpaEnvironmentFactory.instance.pageProviderRepository.value
+            .getAllProviders()
+            .mapNotNull { page ->
+                if (page is SearchablePage) {
+                    page.createSpaSearchIndexablePage(
+                        getPageTitleForSearch = page::getPageTitleForSearch,
+                        getSearchItems = page::getSearchItems,
+                    )
+                } else null
+            }
     }
 
     companion object {
         private const val TAG = "SpaSearchRepository"
 
-        fun SettingsPageProvider.createSearchIndexableData(
-            intentAction: String,
-            intentTargetClass: String,
+        private fun SettingsPageProvider.createSpaSearchIndexablePage(
             getPageTitleForSearch: (context: Context) -> String,
-            searchItemsProvider: (context: Context) -> List<SearchItem>,
-        ): SearchIndexableData {
-            val key =
+            getSearchItems: (context: Context) -> List<SearchItem>,
+        ): SpaSearchIndexablePage {
+            val searchLandingKey =
                 SpaSearchLandingKey.newBuilder()
                     .setSpaPage(SpaSearchLandingSpaPage.newBuilder().setDestination(name))
                     .build()
-            val indexableClass = this::class.java
-            val searchIndexProvider = searchIndexProviderOf { context ->
+            return SpaSearchIndexablePage(targetClass = this::class.java) { context ->
                 val pageTitle = getPageTitleForSearch(context)
-                searchItemsProvider(context).map { searchItem ->
-                    createSearchIndexableRaw(
-                        context = context,
-                        spaSearchLandingKey = key,
-                        itemTitle = searchItem.itemTitle,
-                        indexableClass = indexableClass,
+                getSearchItems(context).map { searchItem ->
+                    SpaSearchIndexableItem(
+                        searchLandingKey = searchLandingKey,
                         pageTitle = pageTitle,
-                        intentAction = intentAction,
-                        intentTargetClass = intentTargetClass,
+                        itemTitle = searchItem.itemTitle,
                         keywords = searchItem.keywords,
                     )
                 }
             }
-            return SearchIndexableData(indexableClass, searchIndexProvider)
         }
-
-        fun searchIndexProviderOf(
-            getDynamicRawDataToIndex: (context: Context) -> List<SearchIndexableRaw>
-        ) =
-            object : Indexable.SearchIndexProvider {
-                override fun getXmlResourcesToIndex(
-                    context: Context,
-                    enabled: Boolean,
-                ): List<SearchIndexableResource> = emptyList()
-
-                override fun getRawDataToIndex(
-                    context: Context,
-                    enabled: Boolean,
-                ): List<SearchIndexableRaw> = emptyList()
-
-                override fun getDynamicRawDataToIndex(
-                    context: Context,
-                    enabled: Boolean,
-                ): List<SearchIndexableRaw> = getDynamicRawDataToIndex(context)
-
-                override fun getNonIndexableKeys(context: Context): List<String> = emptyList()
-            }
-
-        fun createSearchIndexableRaw(
-            context: Context,
-            spaSearchLandingKey: SpaSearchLandingKey,
-            itemTitle: String,
-            indexableClass: Class<*>,
-            pageTitle: String,
-            intentAction: String,
-            intentTargetClass: String,
-            keywords: String? = null,
-        ) =
-            SearchIndexableRaw(context).apply {
-                key = spaSearchLandingKey.encodeToString()
-                title = itemTitle
-                this.keywords = keywords
-                this.intentAction = intentAction
-                this.intentTargetClass = intentTargetClass
-                packageName = context.packageName
-                className = indexableClass.name
-                screenTitle = pageTitle
-            }
     }
 }
