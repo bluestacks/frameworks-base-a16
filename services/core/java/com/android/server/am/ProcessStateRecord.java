48729b2f4ee8b8c3cde25d45fb0e6d45d31e2a90
David Stevens <stevensd@google.com>
2025-05-08 15:51:30 +0900

    Clean up logic for killing bg restricted apps
    Clean up logic around killing cached processes in idle, background
    restricted UIDs. ProcessStateRecord includes flags for marking a process
    as exempt from this type of killing, but this functionality is not and
    has never been used. Remove the flags that are always false to simplify
    the code. Also, rename mLastCanKillOnBgRestrictedAndIdleTime to
    mLastCachedTime, since that is what it's actually tracking.
    
    Test: atest ActivityManagerTest#testKillAppIfBgRestrictedCachedIdle
    Flag: EXEMPT refactor
    Change-Id: I401661d4066404f44f17235114acddee311d29c3

--- services/core/java/com/android/server/am/ProcessStateRecord.java
+++ services/core/java/com/android/server/am/ProcessStateRecord.java
@@ -379,32 +379,17 @@ final class ProcessStateRecord {
     private long mLastInvisibleTime;
 
     /**
-     * Whether or not this process could be killed when it's in background restricted mode
-     * and cached &amp; idle state.
-     */
-    @GuardedBy("mService")
-    private boolean mNoKillOnBgRestrictedAndIdle;
-
-    /**
      * Last set value of {@link #isCached()}.
      */
     @GuardedBy("mService")
     private boolean mSetCached;
 
     /**
-     * Last set value of {@link #mNoKillOnBgRestrictedAndIdle}.
+     * When the proc became cached. Used to debounce killing bg restricted apps in
+     * an idle UID.
      */
     @GuardedBy("mService")
-    private boolean mSetNoKillOnBgRestrictedAndIdle;
-
-    /**
-     * The last time when the {@link #mNoKillOnBgRestrictedAndIdle} is false and the
-     * {@link #isCached()} is true, and either the former state is flipping from true to false
-     * when latter state is true, or the latter state is flipping from false to true when the
-     * former state is false.
-     */
-    @GuardedBy("mService")
-    private @ElapsedRealtimeLong long mLastCanKillOnBgRestrictedAndIdleTime;
+    private @ElapsedRealtimeLong long mLastCachedTime;
 
     // Below are the cached task info for OomAdjuster only
     private static final int VALUE_INVALID = -1;
@@ -1272,16 +1257,6 @@ final class ProcessStateRecord {
     }
 
     @GuardedBy("mService")
-    void setNoKillOnBgRestrictedAndIdle(boolean shouldNotKill) {
-        mNoKillOnBgRestrictedAndIdle = shouldNotKill;
-    }
-
-    @GuardedBy("mService")
-    boolean shouldNotKillOnBgRestrictedAndIdle() {
-        return mNoKillOnBgRestrictedAndIdle;
-    }
-
-    @GuardedBy("mService")
     void setSetCached(boolean cached) {
         mSetCached = cached;
     }
@@ -1292,24 +1267,14 @@ final class ProcessStateRecord {
     }
 
     @GuardedBy("mService")
-    void setSetNoKillOnBgRestrictedAndIdle(boolean shouldNotKill) {
-        mSetNoKillOnBgRestrictedAndIdle = shouldNotKill;
-    }
-
-    @GuardedBy("mService")
-    boolean isSetNoKillOnBgRestrictedAndIdle() {
-        return mSetNoKillOnBgRestrictedAndIdle;
-    }
-
-    @GuardedBy("mService")
-    void setLastCanKillOnBgRestrictedAndIdleTime(@ElapsedRealtimeLong long now) {
-        mLastCanKillOnBgRestrictedAndIdleTime = now;
+    void setLastCachedTime(@ElapsedRealtimeLong long now) {
+        mLastCachedTime = now;
     }
 
     @ElapsedRealtimeLong
     @GuardedBy("mService")
-    long getLastCanKillOnBgRestrictedAndIdleTime() {
-        return mLastCanKillOnBgRestrictedAndIdleTime;
+    long getLastCachedTime() {
+        return mLastCachedTime;
     }
 
     public void setCacheOomRankerRss(long rss, long rssTimeMs) {
