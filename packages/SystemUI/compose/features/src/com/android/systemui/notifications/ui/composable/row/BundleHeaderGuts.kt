52f72e8ce0e376af7712f5a91e49759ed31a3b11
Andreas Miko <amiko@google.com>
2025-05-08 16:15:50 +0200

    Polish BundleHeader guts icons
    Bug: 409748420
    Test: Screenshot test
    Flag: com.android.systemui.notification_bundle_ui
    Change-Id: I93a6c75bbe3d730ff57c49207d3b9cfa897c0ea2

--- packages/SystemUI/compose/features/src/com/android/systemui/notifications/ui/composable/row/BundleHeaderGuts.kt
+++ packages/SystemUI/compose/features/src/com/android/systemui/notifications/ui/composable/row/BundleHeaderGuts.kt
@@ -28,6 +28,9 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -39,7 +42,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -86,10 +88,9 @@ private fun TopRow(viewModel: BundleHeaderGutsViewModel, modifier: Modifier = Mo
         )
 
         Image(
-            painter = painterResource(R.drawable.ic_settings_24dp),
-            // TODO(b/409748420): Add correct CD
+            imageVector = Icons.Default.Settings,
             contentDescription =
-                stringResource(com.android.systemui.res.R.string.notification_more_settings),
+                stringResource(com.android.systemui.res.R.string.accessibility_long_click_tile),
             modifier =
                 Modifier.size(24.dp)
                     .clickable(
@@ -139,14 +140,12 @@ private fun ContentRow(viewModel: BundleHeaderGutsViewModel, modifier: Modifier 
             onCheckedChange = { viewModel.switchState = !viewModel.switchState },
             thumbContent = {
                 Icon(
-                    // TODO(b/409748420): Add correct icon
-                    painter = painterResource(R.drawable.ic_check_circle_24px),
+                    imageVector = Icons.Default.Check,
                     contentDescription = null,
                     modifier = Modifier.size(SwitchDefaults.IconSize),
                     tint = MaterialTheme.colorScheme.onSecondaryContainer,
                 )
             },
-            // TODO(b/409748420): Implement correct switch colors
         )
     }
 }
