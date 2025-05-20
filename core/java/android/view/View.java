fe0b65f1265ef870e25f17314bcca4235d79f94b
Biswarup Pal <biswarupp@google.com>
2025-05-20 03:16:33 -0700

    Revert "Migrate usages of ViewConfiguration static methods"
    This reverts commit 29122b33222e08bbed59aff8e341382babb962df.
    
    Reason for revert: b/418347997
    
    Bug: 370720522
    Change-Id: I43ca6071ffe32af0a39dce4ec2925a1cf0f8414a

--- core/java/android/view/View.java
+++ core/java/android/view/View.java
@@ -6007,7 +6007,8 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
 
         final ViewConfiguration configuration = ViewConfiguration.get(context);
         mTouchSlop = configuration.getScaledTouchSlop();
-
+        mTapTimeoutMillis = Flags.viewconfigurationApis()
+                ? configuration.getTapTimeoutMillis() : ViewConfiguration.getTapTimeout();
         mAmbiguousGestureMultiplier = configuration.getScaledAmbiguousGestureMultiplier();
 
         setOverScrollMode(OVER_SCROLL_IF_CONTENT_SCROLLS);
@@ -17567,7 +17568,7 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
                         setPressed(true, x, y);
                     }
                     checkForLongClick(
-                            getLongPressTimeoutMillis(),
+                            ViewConfiguration.getLongPressTimeout(),
                             x,
                             y,
                             // This is not a touch gesture -- do not classify it as one.
@@ -18375,7 +18376,7 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
 
                     if (!clickable) {
                         checkForLongClick(
-                                getLongPressTimeoutMillis(),
+                                ViewConfiguration.getLongPressTimeout(),
                                 x,
                                 y,
                                 TOUCH_GESTURE_CLASSIFIED__CLASSIFICATION__LONG_PRESS);
@@ -18403,7 +18404,7 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
                         // Not inside a scrolling container, so show the feedback right away
                         setPressed(true, x, y);
                         checkForLongClick(
-                                getLongPressTimeoutMillis(),
+                                ViewConfiguration.getLongPressTimeout(),
                                 x,
                                 y,
                                 TOUCH_GESTURE_CLASSIFIED__CLASSIFICATION__LONG_PRESS);
@@ -18437,7 +18438,7 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
                             // just extend the timeout here, in case the classification
                             // stays ambiguous.
                             removeLongPressCallback();
-                            long delay = (long) (getLongPressTimeoutMillis()
+                            long delay = (long) (ViewConfiguration.getLongPressTimeout()
                                     * mAmbiguousGestureMultiplier);
                             // Subtract the time already spent
                             delay -= event.getEventTime() - event.getDownTime();
@@ -18523,12 +18524,6 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
         if (mPendingCheckForLongPress != null) {
             removeCallbacks(mPendingCheckForLongPress);
         }
-    }
-
-    private int getLongPressTimeoutMillis() {
-        return Flags.viewconfigurationApis()
-                ? ViewConfiguration.get(getContext()).getLongPressTimeoutMillis()
-                : ViewConfiguration.getLongPressTimeout();
     }
 
     /**
@@ -31715,7 +31710,8 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
         public void run() {
             mPrivateFlags &= ~PFLAG_PREPRESSED;
             setPressed(true, x, y);
-            final int delay = getLongPressTimeoutMillis() - mTapTimeoutMillis;
+            final long delay =
+                    (long) ViewConfiguration.getLongPressTimeout() - mTapTimeoutMillis;
             checkForLongClick(delay, x, y, TOUCH_GESTURE_CLASSIFIED__CLASSIFICATION__LONG_PRESS);
         }
     }
