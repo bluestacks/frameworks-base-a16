f46602c2c0dfc2340e526136259f73e8d82af3df
Liana Kazanova (xWF) <lkazanova@google.com>
2025-05-23 15:43:53 -0700

    Revert "Update TestableContext annotation from ClassRule to Rule"
    This reverts commit b83554853f28a07ce5edef2aec1d4abb26c5aa89.
    
    Reason for revert: DroidMonitor: Potential culprit for http://b/419887567 - verifying through ABTD before revert submission. This is part of the standard investigation process, and does not mean your CL will be reverted.
    
    Bug: 414928899
    Bug: 419887567
    Change-Id: I8e053add06a1fe9509f8c864138c011435c92840

--- services/tests/mockingservicestests/src/com/android/server/power/FaceDownDetectorTest.java
+++ services/tests/mockingservicestests/src/com/android/server/power/FaceDownDetectorTest.java
@@ -37,6 +37,7 @@ import androidx.test.platform.app.InstrumentationRegistry;
 import com.android.modules.utils.testing.TestableDeviceConfig;
 
 import org.junit.Before;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -50,7 +51,7 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 public class FaceDownDetectorTest {
-    @Rule
+    @ClassRule
     public static final TestableContext sContext = new TestableContext(
             InstrumentationRegistry.getInstrumentation().getTargetContext(), null);
     @Rule
