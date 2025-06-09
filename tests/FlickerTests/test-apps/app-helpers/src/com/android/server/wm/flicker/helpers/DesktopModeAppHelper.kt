008a8992559155f8ef9a3d8eaceffbebda42e4e1
CHIA-HSUAN HSU <chiahsuanh@google.com>
2025-06-09 05:10:35 +0000

    b/399823963 Add new test case for exit desktop mode to split screen with app header
    Test: USE_RBE=false atest
    WMShellFlickerTestsDesktopMode:com.android.wm.shell.flicker.fundamentals.ExitDesktopToSplitScreenWithAppHeaderMenuFlickerTest
    -c (http://ab/I35900010406378033)
    Video: http://recall/-/bWDabmlKdEDgYWjkQ7uJrl
    Flag: EXEMPT add new test case
    Bug: 399823963
    
    Change-Id: I6f40621e6da1145f6b8fbed54521ff0161c78851

--- tests/FlickerTests/test-apps/app-helpers/src/com/android/server/wm/flicker/helpers/DesktopModeAppHelper.kt
+++ tests/FlickerTests/test-apps/app-helpers/src/com/android/server/wm/flicker/helpers/DesktopModeAppHelper.kt
@@ -592,6 +592,21 @@ open class DesktopModeAppHelper(private val innerHelper: IStandardAppHelper) :
         wmHelper.StateSyncBuilder().withAppTransitionIdle().waitForAndVerify()
     }
 
+    fun exitDesktopModeToSplitScreenWithAppHeader(wmHelper:WindowManagerStateHelper) {
+        val openMenuButton = getDesktopAppViewByRes(OPEN_MENU_BUTTON)
+        openMenuButton?.click()
+        wmHelper.StateSyncBuilder().withAppTransitionIdle().waitForAndVerify()
+
+        val pill = getDesktopAppViewByRes(PILL_CONTAINER)
+        val splitScreenModeButton =
+            pill
+                ?.children
+                ?.find { it.resourceName.endsWith(SPLIT_SCREEN_BUTTON) }
+
+        splitScreenModeButton?.click()
+        wmHelper.StateSyncBuilder().withAppTransitionIdle().waitForAndVerify()
+    }
+
     fun restartFromAppHandleMenu(wmHelper: WindowManagerStateHelper) {
         val openMenuButton = getDesktopAppViewByRes(OPEN_MENU_BUTTON)
         openMenuButton?.click()
@@ -719,6 +734,7 @@ open class DesktopModeAppHelper(private val innerHelper: IStandardAppHelper) :
         const val RESTART_BUTTON: String = "handle_menu_restart_button"
         const val RESTART_DIALOG_RESTART_BUTTON: String = "letterbox_restart_dialog_restart_button"
         const val FULL_SCREEN_BUTTON: String = "fullscreen_button"
+        const val SPLIT_SCREEN_BUTTON: String = "split_screen_button"
         val caption: BySelector
             get() = By.res(SYSTEMUI_PACKAGE, CAPTION)
         // In DesktopMode, window snap can be done with just a single window. In this case, the
