3698ffe96ad1451fc9ffd3d1027fd58e3f25b77e
Caitlin Shkuratov <caitlinshk@google.com>
2025-06-17 16:29:51 +0000

    [SB] Allow carrier merged conns to inflate signal strength - Kairos
    A re-implementation of If250be73b3b86ac31237d4e11327c6233bc0221f but in
    the Kairos pipeline.
    
    Bug: 423686800
    Flag: com.android.systemui.status_bar_inflate_carrier_merged
    Flag: com.android.systemui.status_bar_mobile_icon_kairos
    Test: atest CarrierMergedConnectionRepositoryKairosAdapterTest
    Test: atest CarrierMergedConnectionRepositoryKairosTest
    
    Test: With new flag on and Kairos on:
     (adb shell am broadcast -a com.android.systemui.demo -e command
    network -e wifi carriermerged -e slot 2 -e level 4 -e numlevels 5 -e
    inflate false) -> verify triangle with W+ displays with 4/4 bars filled
    in (numlevels=5 means the level can be in range [0-4])
     (adb shell am broadcast -a com.android.systemui.demo -e command
    network -e wifi carriermerged -e slot 2 -e level 4 -e numlevels 5 -e
    inflate true) -> verify triangle with W+ displays with 5/5 bars filled
    in
    
    Test: With new flag off: Verify those same test cases, but that they all
    show 4/4 bars filled in
    
    Change-Id: Ib60d22310687efe78e9e880838113cde2fde80b0

--- packages/SystemUI/multivalentTests/src/com/android/systemui/statusbar/pipeline/mobile/data/repository/prod/CarrierMergedConnectionRepositoryKairosAdapterTest.kt
+++ packages/SystemUI/multivalentTests/src/com/android/systemui/statusbar/pipeline/mobile/data/repository/prod/CarrierMergedConnectionRepositoryKairosAdapterTest.kt
@@ -27,7 +27,6 @@ import com.android.systemui.statusbar.pipeline.wifi.data.repository.fake
 import com.android.systemui.statusbar.pipeline.wifi.data.repository.wifiRepository
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
-import org.junit.Ignore
 import org.junit.runner.RunWith
 import org.mockito.Mockito
 
@@ -35,7 +34,6 @@ import org.mockito.Mockito
 @SmallTest
 @RunWith(AndroidJUnit4::class)
 @android.platform.test.annotations.EnabledOnRavenwood
-@Ignore("b/423686800") // Re-enable once carrier merged inflation is implemented in Kairos
 class CarrierMergedConnectionRepositoryKairosAdapterTest :
     CarrierMergedConnectionRepositoryTestBase() {
 
@@ -54,6 +52,7 @@ class CarrierMergedConnectionRepositoryKairosAdapterTest :
                             SUB_ID,
                             logger,
                             telephonyManager,
+                            systemUiCarrierConfig,
                             kosmos.wifiRepository.fake,
                             isInEcmMode = stateOf(false),
                         )
