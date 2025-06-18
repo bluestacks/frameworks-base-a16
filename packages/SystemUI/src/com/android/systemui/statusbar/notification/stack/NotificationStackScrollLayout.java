9aa57f7cb854c6104ab5a90025b719b4862f3b2a
Ibrahim Yilmaz <iyz@google.com>
2025-06-18 18:38:47 +0000

    [SwipeHelper] remove not swiped out notification from swiped out list
    It is expected that Swiped out view is moved to transientview first and after that swipe out animation runs. However, this animation is not part of NSSL animations and it runs independently.
    This animation starts before NSSL removes this notification and get cancelled due to some updates. In this edge case, this view is moved to transientview and remain on the shade.
    
    This CL removes this notification from swiped out list when it is not swiped out and leaves removal responsibility to NSSL.
    
    Fixes: 415279408
    Test: Post  a notification with notify with delay and have many updates. During updates try to swipe out quickly.
    Flag: EXEMPT bug fix
    Change-Id: I18a8b95501fd93f41571dd71ba5a20ab24a87c9c

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayout.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayout.java
@@ -6084,6 +6084,18 @@ public class NotificationStackScrollLayout
         }
     }
 
+    void removeSwipedOutView(View v) {
+        logRemoveSwipedOutView(v);
+        mSwipedOutViews.remove(v);
+    }
+
+    private void logRemoveSwipedOutView(View v) {
+        if (mLogger != null && v instanceof ExpandableNotificationRow row) {
+            mLogger.logRemoveSwipedOutView(row.getLoggingKey(), mClearAllInProgress);
+        }
+    }
+
+
     void onSwipeBegin(View viewSwiped) {
         if (!(viewSwiped instanceof ExpandableNotificationRow)) {
             return;
