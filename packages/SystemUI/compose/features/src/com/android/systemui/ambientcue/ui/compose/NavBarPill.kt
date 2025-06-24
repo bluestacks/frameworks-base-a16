b25c5b8fc49bed76c0d773c32ef437732d3afb21
Lucas Dupin <dupin@google.com>
2025-06-23 18:59:41 -0700

    Allow for MR text to grow with font scaling
    Test:  atest NavBarPillScreenshotTest
    Fixes: 424717224
    Flag: EXEMPT bugfix
    Change-Id: I7e0cfc909a3799916a9f31c01db36fdfffc4cef3

--- packages/SystemUI/compose/features/src/com/android/systemui/ambientcue/ui/compose/NavBarPill.kt
+++ packages/SystemUI/compose/features/src/com/android/systemui/ambientcue/ui/compose/NavBarPill.kt
@@ -33,6 +33,7 @@ import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -44,7 +45,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -145,10 +145,15 @@ fun NavBarPill(
             label = "expansion",
         )
     val config = LocalConfiguration.current
-    val isBoldTextEnabled by remember { derivedStateOf { config.fontWeightAdjustment > 0 } }
+    val isBoldTextEnabled = config.fontWeightAdjustment > 0
+    val fontScale = config.fontScale
     val actionTextStyle =
         MaterialTheme.typography.labelMedium.copy(
-            fontWeight = if (isBoldTextEnabled) FontWeight.Bold else FontWeight.Medium
+            fontWeight = if (isBoldTextEnabled) FontWeight.Bold else FontWeight.Medium,
+            fontSize =
+                with(density) {
+                    (MaterialTheme.typography.labelMedium.fontSize.value * fontScale).dp.toSp()
+                },
         )
 
     Box(
@@ -254,7 +259,8 @@ fun NavBarPill(
                                                     Color.Transparent
                                                 }
                                             )
-                                            .padding(4.dp),
+                                            .height(28.dp)
+                                            .padding(start = 6.dp, end = 6.dp),
                                 ) {
                                     Image(
                                         painter = rememberDrawablePainter(action.icon.drawable),
