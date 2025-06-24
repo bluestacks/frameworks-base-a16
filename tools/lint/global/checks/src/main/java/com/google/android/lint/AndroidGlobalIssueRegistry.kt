cad13debadc4f1b63a6a25f3a315deba4a3e1a94
Ömer Faruk Yılmaz <ofy@google.com>
2025-06-24 11:27:12 +0900

    Add RequiresPermissionDetector
    Bug: 409768276
    Test: atest --host AndroidGlobalLintCheckerTest:com.google.android.lint.aidl.RequiresPermissionDetectorTest
    Flag: EXEMPT new lint
    Change-Id: Id4b627d75ca1c455af1c3bb8c9c44e76a9ab564e

--- tools/lint/global/checks/src/main/java/com/google/android/lint/AndroidGlobalIssueRegistry.kt
+++ tools/lint/global/checks/src/main/java/com/google/android/lint/AndroidGlobalIssueRegistry.kt
@@ -21,6 +21,7 @@ import com.android.tools.lint.client.api.Vendor
 import com.android.tools.lint.detector.api.CURRENT_API
 import com.google.android.lint.aidl.EnforcePermissionDetector
 import com.google.android.lint.aidl.PermissionAnnotationDetector
+import com.google.android.lint.aidl.RequiresPermissionDetector
 import com.google.android.lint.aidl.SimpleManualPermissionEnforcementDetector
 import com.google.android.lint.aidl.SimpleRequiresNoPermissionDetector
 import com.google.android.lint.flags.CheckFlagsRuleDetector
@@ -34,6 +35,8 @@ class AndroidGlobalIssueRegistry : IssueRegistry() {
             EnforcePermissionDetector.ISSUE_MISMATCHING_ENFORCE_PERMISSION,
             EnforcePermissionDetector.ISSUE_ENFORCE_PERMISSION_HELPER,
             EnforcePermissionDetector.ISSUE_MISUSING_ENFORCE_PERMISSION,
+            RequiresPermissionDetector.ISSUE_MISSING_OR_MISMATCHED_REQUIRES_PERMISSION_ANNOTATION,
+            RequiresPermissionDetector.ISSUE_INCORRECT_REQUIRES_PERMISSION_PROPAGATION,
             PermissionAnnotationDetector.ISSUE_MISSING_PERMISSION_ANNOTATION,
             SimpleManualPermissionEnforcementDetector.ISSUE_SIMPLE_MANUAL_PERMISSION_ENFORCEMENT,
             SimpleRequiresNoPermissionDetector.ISSUE_SIMPLE_REQUIRES_NO_PERMISSION,
