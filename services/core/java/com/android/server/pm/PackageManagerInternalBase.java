cbc88e39ceea53641c3d418925eebe00d104f8c2
Sudheer Shanka <sudheersai@google.com>
2025-06-26 23:16:34 -0700

    Allow specifying which packages to send a broadcast to.
    Add BroadcastOptions.setIncludedPackages() that allows
    specifying which packages to send a broadcast to.
    
    Bug: 421215310
    Test: atest tests/app/BroadcastsTest/src/android/app/cts/broadcasts/BroadcastOptionsIncludedPackagesTest.java
    Flag: EXEMPT hidden api - there shouldn't be any effect until a client uses it
    Change-Id: I32d5ec6b031cfefb3a48868e5f243449a41a6c1e

--- services/core/java/com/android/server/pm/PackageManagerInternalBase.java
+++ services/core/java/com/android/server/pm/PackageManagerInternalBase.java
@@ -53,6 +53,7 @@ import android.util.ArraySet;
 import android.util.SparseArray;
 
 import com.android.internal.pm.pkg.component.ParsedMainComponent;
+import com.android.internal.util.ArrayUtils;
 import com.android.server.pm.dex.DexManager;
 import com.android.server.pm.permission.PermissionManagerServiceInternal;
 import com.android.server.pm.pkg.AndroidPackage;
@@ -329,9 +330,18 @@ abstract class PackageManagerInternalBase extends PackageManagerInternal {
     @Deprecated
     public final List<ResolveInfo> queryIntentReceivers(
             Intent intent, String resolvedType, @PackageManager.ResolveInfoFlagsBits long flags,
-            int filterCallingUid, int callingPid, int userId, boolean forSend) {
-        return getResolveIntentHelper().queryIntentReceiversInternal(snapshot(), intent,
+            int filterCallingUid, int callingPid, int userId, boolean forSend,
+            String[] includedPackages) {
+        final List<ResolveInfo> result = getResolveIntentHelper().queryIntentReceiversInternal(
+                snapshot(), intent,
                 resolvedType, flags, userId, filterCallingUid, callingPid, forSend);
+        // TODO: b/428262517 - filter out packages that are not in includedPackages close to
+        // intent resolution.
+        if (includedPackages != null) {
+            result.removeIf(resolvedInfo -> !ArrayUtils.contains(
+                    includedPackages, resolvedInfo.activityInfo.packageName));
+        }
+        return result;
     }
 
     @Override
