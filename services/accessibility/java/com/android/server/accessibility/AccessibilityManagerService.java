3c82159558dbaab4ea82b3510ccc97d622eee135
Asmita Poddar <asmitapoddar@google.com>
2025-07-02 11:58:31 +0000

    Remove flag KeyboardA11yMouseKeys
    Bug: 409610617
    Test: Presubmit
    Flag: EXEMPT cleanup
    Change-Id: Idb1c0f9ff7c3157a7572fa3f113cac770a1b5f2d

--- services/accessibility/java/com/android/server/accessibility/AccessibilityManagerService.java
+++ services/accessibility/java/com/android/server/accessibility/AccessibilityManagerService.java
@@ -46,7 +46,6 @@ import static android.view.accessibility.AccessibilityManager.FlashNotificationR
 
 import static com.android.hardware.input.Flags.enableTalkbackAndMagnifierKeyGestures;
 import static com.android.hardware.input.Flags.enableVoiceAccessKeyGestures;
-import static com.android.hardware.input.Flags.keyboardA11yMouseKeys;
 import static com.android.internal.accessibility.AccessibilityShortcutController.ACCESSIBILITY_HEARING_AIDS_COMPONENT_NAME;
 import static com.android.internal.accessibility.AccessibilityShortcutController.MAGNIFICATION_COMPONENT_NAME;
 import static com.android.internal.accessibility.AccessibilityShortcutController.MAGNIFICATION_CONTROLLER_NAME;
@@ -6303,9 +6302,6 @@ public class AccessibilityManagerService extends IAccessibilityManager.Stub
     }
 
     boolean readMouseKeysEnabledLocked(AccessibilityUserState userState) {
-        if (!keyboardA11yMouseKeys()) {
-            return false;
-        }
         final boolean isMouseKeysEnabled =
                 Settings.Secure.getIntForUser(mContext.getContentResolver(),
                 Settings.Secure.ACCESSIBILITY_MOUSE_KEYS_ENABLED, 0, userState.mUserId) == 1;
