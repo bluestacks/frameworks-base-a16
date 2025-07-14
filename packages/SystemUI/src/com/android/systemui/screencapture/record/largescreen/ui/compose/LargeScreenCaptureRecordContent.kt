3389ce1a4425c87ea119d2634207dcbe8c92a5e7
Anton Potapov <apotapov@google.com>
2025-07-14 15:43:45 +0100

    Add screen recording parameters UI
    Flag: com.android.systemui.new_screen_record_toolbar
    Test: manual on foldable
    Bug: 428686600
    Change-Id: I6d9106d1a4be2ce654c10b25b578597f7a6aa016

--- packages/SystemUI/src/com/android/systemui/screencapture/record/largescreen/ui/compose/LargeScreenCaptureRecordContent.kt
+++ packages/SystemUI/src/com/android/systemui/screencapture/record/largescreen/ui/compose/LargeScreenCaptureRecordContent.kt
@@ -18,10 +18,12 @@ package com.android.systemui.screencapture.record.largescreen.ui.compose
 
 import androidx.compose.runtime.Composable
 import com.android.systemui.lifecycle.rememberViewModel
+import com.android.systemui.screencapture.common.ScreenCaptureScope
 import com.android.systemui.screencapture.common.ui.compose.ScreenCaptureContent
 import com.android.systemui.screencapture.record.largescreen.ui.viewmodel.PreCaptureViewModel
 import javax.inject.Inject
 
+@ScreenCaptureScope
 class LargeScreenCaptureRecordContent
 @Inject
 constructor(private val viewModelFactory: PreCaptureViewModel.Factory) : ScreenCaptureContent {
