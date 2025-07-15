5a26698bbe2d7dde829a19e799a635601ad656a2
CHIA-HSUAN HSU <chiahsuanh@google.com>
2025-07-15 03:14:33 +0000

    Move EnterDesktopFromKeyboardShortcut from /fundamentals into /keyboardhosrtcuts subfolder
    Test: EXEMPT minor test refactoring
    Flag: EXEMPT minor test refactoring
    Bug: 418236032
    Change-Id: If679f671bf0a0b9ab54847ecb76a8104fd692376

--- libs/WindowManager/Shell/tests/e2e/desktopmode/flicker-service/src/com/android/wm/shell/flicker/keyboardshortcuts/EnterDesktopFromKeyboardShortcutFlickerTest.kt
+++ libs/WindowManager/Shell/tests/e2e/desktopmode/flicker-service/src/com/android/wm/shell/flicker/keyboardshortcuts/EnterDesktopFromKeyboardShortcutFlickerTest.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.wm.shell.flicker.fundamentals
+package com.android.wm.shell.flicker.keyboardshortcuts
 
 import androidx.test.filters.RequiresDevice
 import android.tools.NavBar
