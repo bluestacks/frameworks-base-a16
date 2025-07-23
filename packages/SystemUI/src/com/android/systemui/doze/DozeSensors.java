1206a043b64bb53894110c4bec5a18c8fa9ddf63
Jernej Virag <jernej@google.com>
2025-07-23 09:39:02 -0700

    Clean up `register_content_observers_async` flag
    This flag has been released.
    
    Bug: 316922634
    Test: atest SystemUITests
    Flag: EXEMPT flag cleanup
    Change-Id: Ib95f90ff110c09e855273bf1d37bef918d19142e

--- packages/SystemUI/src/com/android/systemui/doze/DozeSensors.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeSensors.java
@@ -441,11 +441,7 @@ public class DozeSensors {
         }
 
         if (!anyListening) {
-            if (Flags.registerContentObserversAsync()) {
-                mSecureSettings.unregisterContentObserverAsync(mSettingsObserver);
-            } else {
-                mSecureSettings.unregisterContentObserverSync(mSettingsObserver);
-            }
+            mSecureSettings.unregisterContentObserverAsync(mSettingsObserver);
         } else if (!mSettingRegistered) {
             for (TriggerSensor s : mTriggerSensors) {
                 s.registerSettingsObserver(mSettingsObserver);
@@ -769,13 +765,8 @@ public class DozeSensors {
 
         public void registerSettingsObserver(ContentObserver settingsObserver) {
             if (mConfigured && !TextUtils.isEmpty(mSetting)) {
-                if (Flags.registerContentObserversAsync()) {
-                    mSecureSettings.registerContentObserverForUserAsync(
-                            mSetting, mSettingsObserver, UserHandle.USER_ALL);
-                } else {
-                    mSecureSettings.registerContentObserverForUserSync(
-                            mSetting, mSettingsObserver, UserHandle.USER_ALL);
-                }
+                mSecureSettings.registerContentObserverForUserAsync(
+                        mSetting, mSettingsObserver, UserHandle.USER_ALL);
             }
         }
 
