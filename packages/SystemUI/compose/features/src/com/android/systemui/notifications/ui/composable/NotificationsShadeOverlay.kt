9f4a9fe9c93f6107a9a6235b61f04a4316ae45f4
Hawkwood <jglazier@google.com>
2025-07-29 16:07:02 +0000

    Move Clock Compose Elements into Plugin Libs
    In order to provide more flexibility to clock layouts when flexiglass
    ships, this change adds compose support to the clock interfaces. This
    lets clocks provide composable functions directly back to SystemUI.
    
    Because of how compose is structured, we couldn't include the library
    in the same way we included previous libraries (like ConstraintLayout).
    Instead, we've needed to include all the libraries statically to fix
    issues with kotlin inline compilation, as well as disabling certain
    proguard optimizations.
    
    We provide the ability for clocks (like weather) to have more distinct
    layouts by providing a dictionary of composable lockscreen elements for
    them to render internally. Clocks must be careful that the elements they
    are attempting to render are rendered elsewhere at the same time or this
    will cause a duplicate key failure within the scene. Currently only the
    smartspace elements are migrated to use this mechanism as they are the
    only elements that the current weather clock needs to integrate with.
    
    Bug: 432451019
    Test: test_gradle_build.sh
    Test: Checked rendering of clocks in flexiglass
    Flag: com.android.systemui.scene_container
    Change-Id: Ib8cd96b0662afd8bf5a84738a67b1d22b1235dbb

--- packages/SystemUI/compose/features/src/com/android/systemui/notifications/ui/composable/NotificationsShadeOverlay.kt
+++ packages/SystemUI/compose/features/src/com/android/systemui/notifications/ui/composable/NotificationsShadeOverlay.kt
@@ -30,8 +30,6 @@ import com.android.compose.animation.scene.UserAction
 import com.android.compose.animation.scene.UserActionResult
 import com.android.internal.jank.InteractionJankMonitor
 import com.android.systemui.dagger.SysUISingleton
-import com.android.systemui.keyguard.ui.composable.blueprint.rememberBurnIn
-import com.android.systemui.keyguard.ui.composable.element.SmallClockElement
 import com.android.systemui.keyguard.ui.viewmodel.KeyguardClockViewModel
 import com.android.systemui.lifecycle.rememberViewModel
 import com.android.systemui.media.controls.ui.composable.MediaCarousel
@@ -65,7 +63,6 @@ constructor(
     private val contentViewModelFactory: NotificationsShadeOverlayContentViewModel.Factory,
     private val shadeSession: SaveableSession,
     private val stackScrollView: Lazy<NotificationScrollView>,
-    private val smallClockElement: SmallClockElement,
     private val keyguardClockViewModel: KeyguardClockViewModel,
     private val mediaCarouselController: MediaCarouselController,
     @Named(QUICK_QS_PANEL) private val mediaHost: Lazy<MediaHost>,
@@ -129,14 +126,7 @@ constructor(
         ) {
             Column {
                 if (isFullWidth) {
-                    val burnIn = rememberBurnIn(keyguardClockViewModel)
-
-                    with(smallClockElement) {
-                        SmallClock(
-                            burnInParams = burnIn.parameters,
-                            onTopChanged = burnIn.onSmallClockTopChanged,
-                        )
-                    }
+                    // TODO(b/432451019): Integrate with smallClock for correct transition
                 }
 
                 MediaCarousel(
