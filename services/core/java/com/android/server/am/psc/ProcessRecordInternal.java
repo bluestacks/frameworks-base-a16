e3b7949a39fc5e7b2c17de6031b998468a555eec
Chih-Yu Huang <akahuang@google.com>
2025-07-30 16:19:29 +0900

    psc: Remove redundant uid field from ProcessRecord
    The `uid` field in `ProcessRecord` was a duplication of the `mUid`
    field already present in its superclass, `ProcessRecordInternal`. This
    patch removes the redundant field from `ProcessRecord` and renames
    `mUid` to `uid` in `ProcessRecordInternal`, making it public. This
    consolidates the process UID definition in the base class.
    
    Bug: 425766486
    Test: m services.core
    Test: atest MockingOomAdjusterTests OomAdjusterTests
    Test: atest FrameworksServicesTestsRavenwood_ProcessStateController
    Flag: EXEMPT pure refactor
    Change-Id: Ie984ad388d0960486e2af6d03982c595408e4e50

--- services/core/java/com/android/server/am/psc/ProcessRecordInternal.java
+++ services/core/java/com/android/server/am/psc/ProcessRecordInternal.java
@@ -227,9 +227,13 @@ public abstract class ProcessRecordInternal {
     private static final boolean TRACE_OOM_ADJ = false;
 
     private final String mProcessName;
-    private final int mUid;
     private String mTrackName;
 
+    /**
+     * The process's actual UID.
+     * This may differ from {@link #getApplicationUid()} if it's an isolated process.
+     */
+    public final int uid;
     public final boolean isolated;     // true if this is a special isolated process
     public final boolean isSdkSandbox; // true if this is an SDK sandbox process
 
@@ -667,9 +671,9 @@ public abstract class ProcessRecordInternal {
 
     public ProcessRecordInternal(String processName, int uid, Object serviceLock, Object procLock) {
         mProcessName = processName;
-        mUid = uid;
-        isSdkSandbox = Process.isSdkSandboxUid(mUid);
-        isolated = Process.isIsolatedUid(mUid);
+        this.uid = uid;
+        isSdkSandbox = Process.isSdkSandboxUid(this.uid);
+        isolated = Process.isIsolatedUid(this.uid);
         mServiceLock = serviceLock;
         mProcLock = procLock;
     }
@@ -1648,7 +1652,7 @@ public abstract class ProcessRecordInternal {
      */
     private String getTrackName() {
         if (mTrackName == null) {
-            mTrackName = "oom:" + mProcessName + "/u" + mUid;
+            mTrackName = "oom:" + mProcessName + "/u" + uid;
         }
         return mTrackName;
     }
