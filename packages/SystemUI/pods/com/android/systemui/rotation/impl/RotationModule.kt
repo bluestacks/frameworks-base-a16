b31a94f5c2b5df44d3f9f9954d28def0487a0d0a
Shivangi Dubey <dshivangi@google.com>
2025-07-31 15:42:50 -0700

    Move RotationPolicyWrapper to SystemUI's rotation pod
    Move RotationPolicyWrapper to SystemUI's rotation pod, so that it could be reused in Launcher for better testability.
    This CL has no behaviour change, but just a package change.
    
    Bug: 430756509
    Flag: EXEMPT Refactor only
    Test: No behaviour change, flash and check if device boots and rotation works as per usual
    Change-Id: I6a38ae2ee2cb39592c4f4f763579411bb7dad17f

--- packages/SystemUI/pods/com/android/systemui/rotation/impl/RotationModule.kt
+++ packages/SystemUI/pods/com/android/systemui/rotation/impl/RotationModule.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 The Android Open Source Project
+ * Copyright (C) 2025 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,16 +14,19 @@
  * limitations under the License.
  */
 
-package com.android.systemui.util.wrapper
+package com.android.systemui.rotation.impl
 
 import com.android.systemui.dagger.SysUISingleton
+import com.android.systemui.rotation.RotationPolicyWrapper
 import dagger.Binds
 import dagger.Module
 
 @Module
-abstract class UtilWrapperModule {
+public abstract class RotationModule {
 
     @Binds
     @SysUISingleton
-    abstract fun bindRotationPolicyWrapper(impl: RotationPolicyWrapperImpl): RotationPolicyWrapper
+    public abstract fun bindRotationPolicyWrapper(
+        impl: RotationPolicyWrapperImpl
+    ): RotationPolicyWrapper
 }
