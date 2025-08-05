415ed0093d201e725266790edeb00fecc7df613d
Lingyu Feng <lingyufeng@google.com>
2025-08-05 01:39:44 -0700

    Enable MIRROR_DISPLAY_NO_ACTIVITY flag override
    Bug: 417098352
    Flag: com.android.window.flags.enable_mirror_display_no_activity
    Test: m
    Change-Id: Ibb53b89476c942cb6524b3bbc36065fa06c1bfd3

--- core/java/android/window/DesktopExperienceFlags.java
+++ core/java/android/window/DesktopExperienceFlags.java
@@ -181,7 +181,7 @@ public enum DesktopExperienceFlags {
             Flags.FLAG_ENABLE_INTERACTIVE_PICTURE_IN_PICTURE),
     ENABLE_KEYBOARD_SHORTCUTS_TO_SWITCH_DESKS(Flags::keyboardShortcutsToSwitchDesks, true,
             Flags.FLAG_KEYBOARD_SHORTCUTS_TO_SWITCH_DESKS),
-    ENABLE_MIRROR_DISPLAY_NO_ACTIVITY(Flags::enableMirrorDisplayNoActivity, false,
+    ENABLE_MIRROR_DISPLAY_NO_ACTIVITY(Flags::enableMirrorDisplayNoActivity, true,
             Flags.FLAG_ENABLE_MIRROR_DISPLAY_NO_ACTIVITY),
     ENABLE_MODALS_FULLSCREEN_WITH_PLATFORM_SIGNATURE(
             Flags::enableModalsFullscreenWithPlatformSignature, true,
