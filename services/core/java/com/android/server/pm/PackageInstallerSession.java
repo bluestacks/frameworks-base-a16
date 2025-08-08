6d171d1290bf2fc76d351f8b633f7328704d4a91
Ivan Chiang <chiangi@google.com>
2025-08-08 06:48:24 +0000

    [PM] Update conditions for updates without user action
    - For Android B, updates without user action are permitted for apps
      targeting API 34+
    - Updated the conditions for SDK version check in
      PackageInstallerSession and associated documentation in
      PackageInstaller.SesionParams
    
    Flag: EXEMPT JAVA DOC
    Bug: 420996962
    Test: atest CtsSilentUpdateHostTestCases
    Change-Id: I6c56897f84e8637f75edb2d3569f2b7b6d2bd2a6

--- services/core/java/com/android/server/pm/PackageInstallerSession.java
+++ services/core/java/com/android/server/pm/PackageInstallerSession.java
@@ -355,14 +355,14 @@ public class PackageInstallerSession extends IPackageInstallerSession.Stub {
     private static final int INCREMENTAL_STORAGE_UNHEALTHY_MONITORING_MS = 60000;
 
     /**
-     * If an app being installed targets {@link Build.VERSION_CODES#TIRAMISU API 33} and above,
-     * the app can be installed without user action.
+     * If an app being installed targets {@link Build.VERSION_CODES#UPSIDE_DOWN_CAKE API 34} and
+     * above, the app can be installed without user action.
      * See {@link PackageInstaller.SessionParams#setRequireUserAction} for other conditions required
      * to be satisfied for a silent install.
      */
     @ChangeId
-    @EnabledSince(targetSdkVersion = Build.VERSION_CODES.TIRAMISU)
-    private static final long SILENT_INSTALL_ALLOWED = 325888262L;
+    @EnabledSince(targetSdkVersion = VERSION_CODES.UPSIDE_DOWN_CAKE)
+    private static final long SILENT_INSTALL_ALLOWED = 420996962L;
 
     /**
      * The system supports pre-approval and update ownership features from
