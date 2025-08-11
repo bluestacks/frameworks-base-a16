215dd5ef254cfdefd4ffee249098c7f878c7a9ff
Md Shahadat Hossain Shahin <shahinmd@google.com>
2025-08-11 14:08:21 +0000

    Consolidate password setting under pin/pattern flag
    Consolidate feature flags by using the existing one for pin and pattern
    changes to control the TEXT_SHOW_PASSWORD default value setting.
    
    Bug: 339270220
    Test: Manual
    Flag: com.android.internal.widget.flags.use_default_visibility_for_sensitive_inputs
    Change-Id: I9e5e7d859c1abc99586e0527be8f6e3500c5e0e0

--- packages/SettingsProvider/src/com/android/providers/settings/SettingsProvider.java
+++ packages/SettingsProvider/src/com/android/providers/settings/SettingsProvider.java
@@ -6760,7 +6760,8 @@ public class SettingsProvider extends ContentProvider {
                 }
 
                 if (currentVersion == 230) {
-                    if (Flags.allowDefaultValueForTextShowPassword()) {
+                    if (com.android.internal.widget.flags.Flags
+                            .useDefaultVisibilityForSensitiveInputs()) {
                         if (systemSettings
                                 .getSettingLocked(Settings.System.TEXT_SHOW_PASSWORD)
                                 .isNull()) {
