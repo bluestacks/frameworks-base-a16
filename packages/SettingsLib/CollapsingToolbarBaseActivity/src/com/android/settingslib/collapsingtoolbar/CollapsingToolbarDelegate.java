504824f9f20c77bcb552934bb0760598d7cd08ee
Mill Chen <millchen@google.com>
2025-08-12 07:15:03 +0000

    [Expressive] disable scrolling in landscape mode
    In the new UI, the collapsing toolbar will be collapsed by default. To
    maintain a consistent user experience, especially when multiple
    fragments are sharing the same activity, we need to disable the
    scrolling functionality of the collapsing toolbar when the device is in
    landscape mode. This will prevent inconsistent behavior and ensure the
    toolbar's state remains predictable.
    
    Bug: 424864280
    Test: visual verify
    Flag: EXEMPT bugfix
    Change-Id: I0a03cc3c3284a91eaf958446ec56de349688a42a

--- packages/SettingsLib/CollapsingToolbarBaseActivity/src/com/android/settingslib/collapsingtoolbar/CollapsingToolbarDelegate.java
+++ packages/SettingsLib/CollapsingToolbarBaseActivity/src/com/android/settingslib/collapsingtoolbar/CollapsingToolbarDelegate.java
@@ -519,7 +519,9 @@ public class CollapsingToolbarDelegate {
                     @Override
                     public boolean canDrag(@NonNull AppBarLayout appBarLayout) {
                         // Header can be scrolling while device in landscape mode and SDK > 33
-                        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.TIRAMISU) {
+                        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.TIRAMISU
+                                || SettingsThemeHelper.isExpressiveTheme(
+                                appBarLayout.getContext())) {
                             return false;
                         } else {
                             return appBarLayout.getResources().getConfiguration().orientation
