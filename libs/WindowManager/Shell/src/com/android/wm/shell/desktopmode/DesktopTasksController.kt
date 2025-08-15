871596c55f6999d03748de12c3c96fbc994b0eb9
Massimo Carli <mcarli@google.com>
2025-08-15 14:48:22 +0000

    [2/n] Clear appBounds on exit from Desktop transition
    To allow activity to be letterboxed when existing from
    Desktop Windowing, it's necessary to reset the appBounds
    from empty to null.
    
    Flag: com.android.window.flags.app_compat_refactoring_set_appbounds_to_null_when_empty
    Bug: 438406944
    Test: atest WMShellUnitTests:DesktopTasksControllerTest
    
    Change-Id: Ib4673b0a859f43e061dbfe3a91dd5bb78107352c

--- libs/WindowManager/Shell/src/com/android/wm/shell/desktopmode/DesktopTasksController.kt
+++ libs/WindowManager/Shell/src/com/android/wm/shell/desktopmode/DesktopTasksController.kt
@@ -4547,6 +4547,11 @@ class DesktopTasksController(
             }
         wct.setWindowingMode(taskInfo.token, targetWindowingMode)
         wct.setBounds(taskInfo.token, Rect())
+
+        if (Flags.appCompatRefactoringSetAppboundsToNullWhenEmpty()) {
+            wct.setAppBounds(taskInfo.token, null)
+        }
+
         if (desktopConfig.useDesktopOverrideDensity) {
             wct.setDensityDpi(taskInfo.token, getDefaultDensityDpi())
         }
