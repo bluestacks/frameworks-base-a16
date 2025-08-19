9eeaca69055e606eebec499c30c04e51490d9750
Matt Pietal <mpietal@google.com>
2025-08-19 14:14:09 +0000

    Add Occluded Scene
    Removes specific scene container logic that would simulate an
    occluded state in flexi, and adds a dedicated scene with STL
    animations. Provides compatibility with KTF by modifying the
    existing animations.
    
    Moves all logic to determine occluded state into
    KeyguardOcclusionInteractor, and also ensures that the tests
    are in the correct keyguard package.
    
    Fixes: 436836037
    Test: atest KeyguardOccludedInteractorTest SceneContainerStartableTest
    Flag: com.android.systemui.scene_container
    Change-Id: I32ff2f8683977cd08011417913a55134d358d71a

--- packages/SystemUI/multivalentTests/src/com/android/systemui/keyguard/ui/viewmodel/LockscreenToGoneTransitionViewModelTest.kt
+++ packages/SystemUI/multivalentTests/src/com/android/systemui/keyguard/ui/viewmodel/LockscreenToGoneTransitionViewModelTest.kt
@@ -37,7 +37,6 @@ import org.junit.runner.RunWith
 
 @SmallTest
 @RunWith(AndroidJUnit4::class)
-@DisableSceneContainer
 class LockscreenToGoneTransitionViewModelTest : SysuiTestCase() {
     private val kosmos = testKosmos()
     private val testScope = kosmos.testScope
@@ -99,6 +98,7 @@ class LockscreenToGoneTransitionViewModelTest : SysuiTestCase() {
         }
 
     @Test
+    @DisableSceneContainer
     fun notificationAlpha_leaveShadeOpen() =
         testScope.runTest {
             val values by collectValues(underTest.notificationAlpha(ViewStateAccessor()))
