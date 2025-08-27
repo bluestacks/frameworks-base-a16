08a8dc80cbc779f57fe2595d6c86d558e5bac705
Jorge Gil <jorgegil@google.com>
2025-08-27 15:58:17 +0000

    Put app handle color animation behind its own flag
    Flag: com.android.window.flags.reenable_app_handle_color_animations
    Bug: 441497981
    Test: open fullscreen task, see color changes without animation
    Change-Id: If0aa2b296aea4769852e99e1bcadbb11e7e63248

--- libs/WindowManager/Shell/src/com/android/wm/shell/windowdecor/viewholder/AppHandleViewHolder.kt
+++ libs/WindowManager/Shell/src/com/android/wm/shell/windowdecor/viewholder/AppHandleViewHolder.kt
@@ -151,7 +151,7 @@ class AppHandleViewHolder(
         isCaptionVisible: Boolean,
     ) {
         setVisibility(isCaptionVisible)
-        if (DesktopExperienceFlags.ENABLE_REENABLE_APP_HANDLE_ANIMATIONS.isTrue) {
+        if (DesktopExperienceFlags.ENABLE_REENABLE_APP_HANDLE_COLOR_ANIMATIONS.isTrue) {
             animator.animateColorChange(getCaptionHandleBarColor(taskInfo))
         } else {
             captionHandle.imageTintList = ColorStateList.valueOf(getCaptionHandleBarColor(taskInfo))
